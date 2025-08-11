994ede731fb558c24063a0b5de5f6eed0e0edec2
Petrányi Dominik <h268432@stud.u-szeged.hu>
2025-08-11 20:16:30 +0200

    request line tests

--- src/request/request_line.rs
+++ src/request/request_line.rs
@@ -45,3 +45,120 @@ impl RequestLine {
         query
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_parse_delete_method() {
+        let row = "DELETE / HTTP/1.1";
+        let result = RequestLine::parse_method(row);
+
+        assert_eq!(RequestMethod::Delete, result);
+    }
+
+    #[test]
+    fn should_parse_get_method() {
+        let row = "GET / HTTP/1.1";
+        let result = RequestLine::parse_method(row);
+
+        assert_eq!(RequestMethod::Get, result);
+    }
+    
+    #[test]
+    fn should_parse_head_method() {
+        let row = "HEAD / HTTP/1.1";
+        let result = RequestLine::parse_method(row);
+
+        assert_eq!(RequestMethod::Head, result);
+    }
+    
+    #[test]
+    fn should_parse_options_method() {
+        let row = "OPTIONS / HTTP/1.1";
+        let result = RequestLine::parse_method(row);
+
+        assert_eq!(RequestMethod::Options, result);
+    }
+    
+    #[test]
+    fn should_parse_patch_method() {
+        let row = "PATCH / HTTP/1.1";
+        let result = RequestLine::parse_method(row);
+
+        assert_eq!(RequestMethod::Patch, result);
+    }
+    
+    #[test]
+    fn should_parse_post_method() {
+        let row = "POST / HTTP/1.1";
+        let result = RequestLine::parse_method(row);
+
+        assert_eq!(RequestMethod::Post, result);
+    }
+    
+    #[test]
+    fn should_parse_put_method() {
+        let row = "PUT / HTTP/1.1";
+        let result = RequestLine::parse_method(row);
+
+        assert_eq!(RequestMethod::Put, result);
+    }
+
+    #[test]
+    #[should_panic]
+    fn should_not_parse_method_of_unknown_type() {
+        let row = "ASDF / HTTP/1.1";
+        let result = RequestLine::parse_method(row);
+    }
+
+    #[test]
+    fn should_parse_path() {
+        let s = "/";
+        let result = RequestLine::parse_path(s);
+
+        assert_eq!(result, s);
+    }
+
+    #[test]
+    fn should_parse_longer_path() {
+        let s = "/some/longer/path";
+        let result = RequestLine::parse_path(s);
+
+        assert_eq!(result, s);
+    }
+
+    #[test]
+    fn should_parse_absolute_uri_path() {
+        let s = "http://www.example.com/index/asdf";
+        let result = RequestLine::parse_path(s);
+
+        assert_eq!(result, s);
+    }
+
+    #[test]
+    fn should_parse_path_with_query() {
+        let s = "/submit-form?user=alice";
+        let expected = "/submit-form";
+        let result = RequestLine::parse_path(s);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn should_parse_query_if_present() {
+        let s = "/submit-form?user=alice";
+        let expected = "user=alice";
+        let result = RequestLine::parse_query(s);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    #[should_panic]
+    fn should_not_parse_query_if_not_present() {
+        let s = "/submit-form";
+        let result = RequestLine::parse_query(s);
+    }
+}
