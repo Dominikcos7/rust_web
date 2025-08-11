d1c48264f051cf3b472be8be5246a5c216d9218e
Petrányi Dominik <h268432@stud.u-szeged.hu>
2025-08-11 18:41:26 +0200

    parse path, query and html version in request line

--- src/request/request_line.rs
+++ src/request/request_line.rs
@@ -4,21 +4,23 @@ use super::request_method::RequestMethod;
 pub struct RequestLine {
     method: RequestMethod,
     path: String,
-    query: String,
+    query: Option<String>,
     http_version: String,
 }
 
 impl RequestLine {
-    pub fn from_string(s: &String) -> Self {
+    pub fn from_string(request_line: &String) -> Self {
+        let parts = request_line.split(" ").collect::<Vec<_>>();
+
         RequestLine {
-            method: Self::parse_method(s),
-            path: String::from(""),
-            query: String::from(""),
-            http_version: String::from(""),
+            method: Self::parse_method(parts[0]),
+            path: Self::parse_path(parts[1]),
+            query: if parts[1].contains("?") {Some(Self::parse_query(parts[1]))} else {None},
+            http_version: parts[2].into(),
         }
     }
 
-    fn parse_method(request_line: &String) -> RequestMethod {
+    fn parse_method(request_line: &str) -> RequestMethod {
         let method = match request_line {
             x if x.to_lowercase().contains("delete") => RequestMethod::Delete,
             x if x.to_lowercase().contains("get") => RequestMethod::Get,
@@ -31,5 +33,15 @@ impl RequestLine {
         };
 
         method
+    }
+
+    fn parse_path(s: &str) -> String {
+        let path: String = s.split("?").collect::<Vec<_>>()[0].into();
+        path
+    }
+
+    fn parse_query(s: &str) -> String {
+        let query: String = s.split("?").collect::<Vec<_>>()[1].into();
+        query
     }
 }
