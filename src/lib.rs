2e4af47488a7c7241dbee0b68b67a1ce407bfc37
Petrányi Dominik <h268432@stud.u-szeged.hu>
2025-08-13 16:10:50 +0200

    send example response with headers

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,21 @@ use std::net::{TcpStream};
 use crate::request::*;
 use crate::response::*;
 
+//todo: insert an url mapper into this function
+//      the mapper will find the controller which will send the response
 pub fn handle_client(mut stream: TcpStream) {
     let request = Request::parse_from_tcp_stream(&stream);
     dbg!(request);
 
+    let body = String::from("some body");
+    let mut headers: HashMap<String, String> = HashMap::new();
+    headers.insert(String::from("Content-Length"), body.len().to_string());
+    headers.insert(String::from("Content-Type"), String::from("text/html"));
+
     let response = Response {
         status_line: StatusLine::from_string("HTTP/1.1 200 OK".to_string()),
-        headers: None,
-        body: Some(String::from("some body")),
+        headers: Some(headers),
+        body: Some(body),
     };
 
     stream.write_all(response.as_bytes().as_slice()).unwrap();
