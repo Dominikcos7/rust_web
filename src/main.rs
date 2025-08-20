420408d9c16ad4b6a6b0db7d701265728343e1ed
Petrányi Dominik <h268432@stud.u-szeged.hu>
2025-08-20 19:01:33 +0200

    use config file for host address

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use std::{collections::HashMap, fs};
+
 fn main() -> std::io::Result<()> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:80")?;
+    let config = fs::read_to_string("config.txt").expect("Should be able to read config file");
+    let config = config.trim();
+    let config_lines = config.split("\r\n").collect::<Vec<_>>();
+    let mut config: HashMap<String, String> = HashMap::new();
+    for line in config_lines {
+        let (k, v) = line.split_once("=").expect("Config line should contain an equal sign (=).");
+        config.insert(k.trim().into(), v.trim().into());
+    }
+
+    let host = config.get("host").expect("Config should include host.");
+    let listener = std::net::TcpListener::bind(host)?;
 
     for stream in listener.incoming() {
         webserver::handle_client(stream?);
